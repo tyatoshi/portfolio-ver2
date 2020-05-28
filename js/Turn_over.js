26cbbd476cbe63e1cceaeaf94edb360d0c5c0274
EC2 Default User <ec2-user@ip-172-31-91-149.ec2.internal>
2020-05-28 05:34:29 +0000

    waku-OK

---
+++ js/Turn_over.js
@@ -0,0 +1,41 @@
+ $(".Turn_over").click(function() {
+  var action = "active animate__animated animate__slideInDown"
+
+   var icon_name = $(this).attr("id");
+   console.log(icon_name);
+   if (icon_name === "h-top") {
+     $(".top").addClass(action);
+     $(".profile").removeClass(action);
+     $(".skill").removeClass(action);
+     $(".portfolio").removeClass(action);
+     $(".contact").removeClass(action);
+   }else if (icon_name === "h-profile") {
+     $(".top").removeClass(action);
+     $(".profile").addClass(action);
+     $(".skill").removeClass(action);
+     $(".portfolio").removeClass(action);
+     $(".contact").removeClass(action);
+   }else if (icon_name === "h-skill") {
+     $(".top").removeClass(action);
+     $(".profile").removeClass(action);
+     $(".skill").addClass(action);
+     $(".portfolio").removeClass(action);
+     $(".contact").removeClass(action);
+   }else if (icon_name === "h-portfolio") {
+     $(".top").removeClass(action);
+     $(".profile").removeClass(action);
+     $(".skill").removeClass(action);
+     $(".portfolio").addClass(action);
+     $(".contact").removeClass(action);
+   }else if (icon_name === "h-contact") {
+     $(".top").removeClass(action);
+     $(".profile").removeClass(action);
+     $(".skill").removeClass(action);
+     $(".portfolio").removeClass(action);
+     $(".contact").addClass(action);
+   }
+
+
+
+ });
+ 
